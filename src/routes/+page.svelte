302f459e62432c0161af00cf431e4482d8cddd70
Subhendu Singh <sps.1431990@gmail.com>
2024-03-06 23:34:31 +0530

    fixed for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang='ts'>
     import anime from 'animejs';
     import * as Tray from "$lib/tray";
+	import { ExternalLink, Twitter, TwitterIcon } from 'lucide-svelte';
 
     const openTray = () => {
         const tray = document.getElementById("tray");
@@ -31,15 +32,20 @@
 <div class="bg-slate-50 h-screen w-screen flex flex-col justify-center items-center gap-4">
     <div class="flex flex-col gap-4 justify-between items-center">
         <h1 class="text-left text-lg text-gray-600 font-semibold">Family's tray system</h1>
-        <a href="https://twitter.com/subhendupsingh" class="text-sm text-blue-500">by subhendupsingh</a>
+        <a href="https://twitter.com/subhendupsingh" class="text-sm text-blue-500 bg-blue-100 px-4 py-2 rounded-lg flex items-center gap-2">
+            <TwitterIcon class="h-4 w-4" />
+            <span>subhendupsingh</span>
+            <ExternalLink class="h-4 w-4" />
+        </a>
     </div>
-    <div class="bg-white shadow-sm border-[1px] border-gray-200 rounded-lg flex justify-center items-center mx-auto my-12 max-w-[692px] h-96 w-full lg:w-1/2 m-6 lg:m-0">
+
+    <div class="bg-white shadow-sm border-[1px] border-gray-200 rounded-lg flex justify-center items-center m-6 max-w-[692px] h-96 w-[90%] lg:w-1/2 lg:m-0">
         <button on:click={()=> openTray()} class="shadow-sm border-[1px] border-gray-200 rounded-md pointer-events-auto px-4 py-2 text-sm font-semibold hover:bg-gray-50 transition-colors duration-300">Open</button>
     </div>
 
-    <div class="absolute h-full w-full flex" id="tray">
+    <div class="absolute h-full w-full flex justify-center" id="tray">
         <div class=" bg-black/30 h-full w-full z-10" id="backdrop"></div>
-        <div class="bg-white bottom-3 left-[40%] p-6 absolute z-20 rounded-[2rem] w-96 max-w-sm" id="tray-container">
+        <div class="bg-white bottom-3 p-6 absolute z-20 rounded-[2rem] w-96 max-w-sm" id="tray-container">
             <!-- Header -->
             <Tray.Header />
 
